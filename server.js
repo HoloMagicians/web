a816633d4e9db2694f0988af4dd76677f762ea35
yutaka <shigure@refy.net>
2016-05-02 20:41:12 +0000

    HoloMagicians template page

--- server.js
+++ server.js
@@ -2,7 +2,16 @@
 
 const express = require('express')
     , app = express()
-    , jadeStatic = require('jade-static');
+    , jadeStatic = require('jade-static')
+    , JsonHelper = require('./lib/json.js');
+
+app.get('/', (req, res) => {
+  JsonHelper(__dirname + '/data/meetups.json', meetups => {
+    JsonHelper(__dirname + '/data/members.json', members => {
+      res.render('index.jade', { meetups: meetups.meetups, members: members.members });
+    });
+  });
+});
 
 app.use(jadeStatic(__dirname + '/views'));
 app.use(express.static(__dirname + '/public'));
