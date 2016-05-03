8b824b7e66a683a5d15109cbb3d16438470c25aa
yutaka <shigure@refy.net>
2016-05-03 08:47:57 +0000

    enable cluster

--- server.js
+++ server.js
@@ -1,22 +1,33 @@
 'use strict';
 
 const express = require('express')
-    , app = express()
     , jadeStatic = require('jade-static')
     , JsonHelper = require('./lib/json.js')
-    , Q = require('q');
+    , Q = require('q')
+    , cluster = require('cluster');
 
-const data = ['/data/meetups.json', '/data/members.json'];
+if(cluster.isMaster){
+  let numCPUs = require('os').cpus().length;
+  for(let i = 0; i < numCPUs; ++i){
+    cluster.fork();
+  }
+  cluster.on('exit', (worker, code, signal) => {
+    cluster.fork();
+  });
+}else{
+  const data = ['/data/meetups.json', '/data/members.json'];
+  const app = express()
 
-app.get('/', (req, res) => {
-  Q.all(data.map(s => Q.nfcall(JsonHelper, __dirname + s)))
-   .then(d => {
-     res.render('index.jade', { meetups: d[0].meetups, members: d[1].members });
-   });
-});
-
-app.use('/api', require('./api/meetups.js'));
-//app.use(jadeStatic(__dirname + '/views'));
-app.use(express.static(__dirname + '/public'));
-
-app.listen(3001);
+  app.get('/', (req, res) => {
+    Q.all(data.map(s => Q.nfcall(JsonHelper, __dirname + s)))
+     .then(d => {
+       res.render('index.jade', { meetups: d[0].meetups, members: d[1].members });
+     });
+  });
+  
+  app.use('/api', require('./api/meetups.js'));
+  //app.use(jadeStatic(__dirname + '/views'));
+  app.use(express.static(__dirname + '/public'));
+  
+  app.listen(3001);
+}
